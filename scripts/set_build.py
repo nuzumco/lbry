aa0432d3819b27c3fb5be5a18a74942682e7bd0b
Jack Robison <jackrobison@lbry.io>
2019-05-14 11:30:43 -0400

    debug travis

--- scripts/set_build.py
+++ scripts/set_build.py
@@ -10,13 +10,16 @@ import sys
 def main():
     build = get_build()
     root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    with open(os.path.join(root_dir, 'lbrynet', 'build_type.py'), 'w') as f:
+    path = os.path.join(root_dir, 'lbrynet', 'build_type.py')
+    print(f"Set build type: {path}", root_dir)
+    with open(path, 'w') as f:
         f.write("BUILD = '{}'\n".format(build))
 
 
 def get_build():
     try:
         tag = subprocess.check_output(['git', 'describe', '--exact-match', '--all']).strip()
+        print(f"Tag info: {tag}")
         if re.match('tags\/v\d+\.\d+\.\d+rc\d+$', tag.decode()):
             print('Build: rc')
             return 'rc'
@@ -28,6 +31,9 @@ def get_build():
     except subprocess.CalledProcessError:
         print("Couldn't determine build type, defaulting to qa.")
         return 'qa'
+    except Exception as err:
+        print(f"Failed to get tag info: {str(type(err))}({str(err)})")
+        raise
 
 
 if __name__ == '__main__':
